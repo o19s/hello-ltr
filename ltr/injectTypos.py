84a4767ae4a2eeb1a5ffe318cffeaf37b0aa75f6
Doug Turnbull <softwaredoug@gmail.com>
2019-03-14 17:12:33 -0400

    Add rounds of typo judgment generation

--- ltr/injectTypos.py
+++ ltr/injectTypos.py
@@ -3,28 +3,39 @@ try:
 except ImportError:
     from .judgments import Judgment, judgmentsFromFile, judgmentsToFile, judgmentsByQid
 
-from random import randint
-import random
+from butterfingers import butterfingers
 
 
-def typoIt(judgmentInFile, judgmentOutFile):
+def typoIt(judgmentInFile, judgmentOutFile, rounds=100):
     currJudgments = [judg for judg in judgmentsFromFile(judgmentInFile)]
     lastQid = currJudgments[-1].qid
     judgDict = judgmentsByQid(currJudgments)
 
-    typoJudgments = []
-    for qid, judglist in judgDict.items():
-        keywords = judglist[0].keywords
-        keywordsWTypo = butterfinger(keywords)
+    existingTypos = set()
 
-        if keywordsWTypo != keywords:
-            newQid = lastQid+1
-            print("%s => %s" % (keywords, keywordsWTypo))
-            lastQid += 1
+    for i in range(0, rounds):
+
+        for qid, judglist in judgDict.items():
+            keywords = judglist[0].keywords
+            keywordsWTypo = butterfingers(keywords)
+
+            if keywordsWTypo != keywords and keywordsWTypo not in existingTypos:
+                newQid = lastQid+1
+                print("%s => %s" % (keywords, keywordsWTypo))
+                lastQid += 1
+                for judg in judglist:
+                    typoJudg = Judgment(grade=judg.grade,
+                                        qid=newQid,
+                                        keywords=keywordsWTypo,
+                                        docId=judg.docId)
+                    currJudgments.append(typoJudg)
+                existingTypos.add(keywordsWTypo)
+
+    judgmentsToFile(filename=judgmentOutFile, judgmentsList=currJudgments)
 
 
 if __name__ == "__main__":
-    typoIt(judgmentInFile='title_judgments.txt', judgmentOutFile=None)
+    typoIt(judgmentInFile='title_judgments.txt', judgmentOutFile='title_fuzzy_judgments.txt')
 
 
     # Clone a judgment, inject random typos
