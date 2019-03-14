5cd1fdc662287398fd208ff1b6e0aced3146ffc3
Doug Turnbull <softwaredoug@gmail.com>
2019-03-13 20:58:19 -0400

    Add training log

--- ltr/train.py
+++ ltr/train.py
@@ -1,9 +1,36 @@
 import os
 import requests
+import re
 
-def trainModel(training, out):
-    cmd = 'java -jar data/RankyMcRankFace.jar -ranker 6 -metric2t DCG@10 -tree 100 -train {} -save {}'.format(training, out)
-    os.system(cmd)
+class TrainingLog:
+    def __init__(self, rawResult):
+        self.rawResult = rawResult
+        self.impacts = {}
+        self.rounds = []
+        lines = rawResult.split('\n')
+
+        impactRe = re.compile(' Feature (\d+) reduced error (.*)')
+        roundsRe = re.compile('(\d+)\s+\| (\d+)')
+        for line in lines:
+            m = re.match(impactRe, line)
+            if m:
+                ftrId = m.group(1)
+                error = float(m.group(2))
+                self.impacts[ftrId] = error
+            m = re.match(roundsRe, line)
+            if m:
+                values = line.split('|')
+                metricTrain = float(values[1])
+                self.rounds.append(metricTrain)
+
+
+
+def trainModel(training, out, metric='DCG@10'):
+
+    cmd = 'java -jar data/RankyMcRankFace.jar -ranker 6 -metric2t {} -tree 100 -train {} -save {}'.format(metric, training, out)
+
+    result = os.popen(cmd).read()
+    return TrainingLog(result)
 
 def saveModel(modelName, modelFile, featureSet):
     model_ep = "http://localhost:9200/_ltr/_model/"
@@ -30,10 +57,14 @@ def saveModel(modelName, modelFile, featureSet):
 
 def run(trainingInFile, modelName, featureSet):
     modelFile='data/{}_model.txt'.format(modelName)
-    trainModel(training=trainingInFile, out=modelFile)
+    trainingLog = trainModel(training=trainingInFile,
+                             out=modelFile)
     saveModel(modelName, modelFile, featureSet)
+    return trainingLog
     print('Done')
 
 
 if __name__ == "__main__":
-    run(trainingInFile='genre_by_date_judgments_train.txt', featureSet='genre', modelName='doug')
+    run(trainingInFile='genre_by_date_judgments_train.txt',
+        featureSet='genre',
+        modelName='doug')
