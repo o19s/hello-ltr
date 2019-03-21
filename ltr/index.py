f4041d814d704a6e24e2200f5cd8dbd4b592ae6e
Doug Turnbull <softwaredoug@gmail.com>
2019-03-20 20:07:56 -0400

    index.run -> rebuild_tmdb

--- ltr/index.py
+++ ltr/index.py
@@ -5,18 +5,12 @@ def reindex(client, schema, movieDict={}, index='tmdb'):
     client.create_index(index, schema)
     client.index_documents(index, movieDict)
 
-def run(client, settings=None):
-    print("RUNNING INDEXING...")
+def rebuild_tmdb(client, settings=None):
     # Recreate the index
     if settings is None:
         with open('data/settings.json') as src:
             settings = json.load(src)
 
-    print("DO IT!")
-
     reindex(client, movieDict=json.load(open('data/tmdb.json')), schema=settings, index='tmdb')
 
     print('Done')
-
-if __name__ == "__main__":
-    run()
