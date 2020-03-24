69413d4ea2c0c330ab16b565e0a2c3ad2a03fe64
Doug Turnbull <softwaredoug@gmail.com>
2020-03-24 17:15:37 -0400

    script using nbclean to clean output from ipynbs

---
+++ clean_ipynbs.py
@@ -0,0 +1,10 @@
+import nbclean as nbc
+import os
+
+if __name__ == "__main__":
+    for fname in os.listdir('.'):
+        if fname.endswith('.ipynb'):
+            print("cleaning %s" % fname)
+            ntbk = nbc.NotebookCleaner(fname)
+            ntbk.clear(kind='output', tag='hide_output')
+            ntbk.save(fname)
