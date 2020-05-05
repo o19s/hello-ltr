5b2b6ab5f7d157d45cb86ba56fee5b52339e3476
Dmitry Kan <dmitry.kan@gmail.com>
2020-05-05 15:45:00 +0300

    #4 added method to keep all models and features

--- ltr/setup.py
+++ ltr/setup.py
@@ -1,3 +1,7 @@
 def setup(client, config, index, featureset):
     client.reset_ltr(index)
     client.create_featureset(index, featureset, config)
+
+
+def setup_keep_old_models_features(client, config, index, featureset):
+    client.create_featureset(index, featureset, config)
