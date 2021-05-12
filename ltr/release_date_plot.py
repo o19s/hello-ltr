0ec95688e74b3aa4e39868a3ca5dfe1ce9fe0527
Ashish Surana <ashishsurana11@gmail.com>
2021-05-11 22:19:40 -0400

    hello-ltr (ES).ipynb now shows top-12 search results for both classic and latest models.
    Also externalized user query term so that one can easily see search results for different query terms.

--- ltr/release_date_plot.py
+++ ltr/release_date_plot.py
@@ -1,26 +1,28 @@
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
-def plot(client):
+def search(client, user_query, model_name):
+    if client.name() == 'elastic':
+        engine_query = {
+            "bool": {
+                "must": {"match_all": {} },
+                "filter": {
+                    "match": {"title": user_query}
+                    }
+            }
+        }
+    else:
+        engine_query = 'title:('+ user_query + ')^0'    
+    return client.model_query('tmdb', model_name, {}, engine_query)
+
+def plot(client, query):
     init_notebook_mode(connected=True)
 
     models = ['classic', 'latest']
     modelData = []
 
-    if client.name() == 'elastic':
-        query = {
-            "bool": {
-                "must": {"match_all": {} },
-                "filter": {
-                    "match": {"title": "batman"}
-                    }
-            }
-        }
-    else:
-        query = 'title:(batman)^0'
-
     for model in models:
-        modelData.append(client.model_query('tmdb', model, {}, query))
+        modelData.append(search(client, query, model))
 
     xAxes = []
     for i in range(len(modelData[0])):
