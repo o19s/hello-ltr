29f60e6fed5e54352169be83c20a858ddd52463e
Nathan Day <nathancday@gmail.com>
2021-08-01 17:34:09 -0400

    add dataframe section to hello ltr

--- ltr/years_as_ratings.py
+++ ltr/years_as_ratings.py
@@ -22,16 +22,20 @@ def get_latest_rating(year):
     else:
         return 0
 
-def synthesize(client, featureSet='release', latestTrainingSetOut='data/latest-training.txt', classicTrainingSetOut='data/classic-training.txt'):
+def synthesize(
+    client,
+    featureSet='release',
+    latestTrainingSetOut='data/latest-training.txt',
+    classicTrainingSetOut='data/classic-training.txt'
+):
     from ltr.judgments import judgments_to_file, Judgment
-    print('Generating ratings for classic and latest model')
     NO_ZERO = False
 
     resp = client.log_query('tmdb', 'release', None)
 
-    # Classic film fan
+    # A classic film fan
     judgments = []
-    print("Generating Classic judgments:")
+    print("Generating 'latest' biased judgments:")
     for hit in resp:
         rating = get_classic_rating(hit['ltr_features'][0])
 
@@ -40,12 +44,13 @@ def synthesize(client, featureSet='release', latestTrainingSetOut='data/latest-t
 
         judgments.append(Judgment(qid=1,docId=hit['id'],grade=rating,features=hit['ltr_features'],keywords=''))
 
+        
     with open(classicTrainingSetOut, 'w') as out:
         judgments_to_file(out, judgments)
 
-    # Latest film fan
+    # A current film fan
     judgments = []
-    print("Generating Recent judgments:")
+    print("Generating 'recent' biased judgments:")
     for hit in resp:
         rating = get_latest_rating(hit['ltr_features'][0])
 
